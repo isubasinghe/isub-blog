d1098ba2d59873fa8e29c4c68c8cdad9bc64e812
Restyled.io <commits@restyled.io>
2020-11-04 10:24:31 +0000

    Restyled by clang-format

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,38 +1,35 @@
-import Head from "next/head";
 import hydrate from "next-mdx-remote/hydrate";
+import Head from "next/head";
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({params}) {
   const matter = require("gray-matter");
-  const { readFile } = require("fs").promises;
+  const {readFile} = require("fs").promises;
   const renderToString = require("next-mdx-remote/render-to-string");
 
-  const { content, data } = matter(
-    await readFile(`./pages/projects/${params.id}.md`, "utf8")
-  );
+  const {content, data} =
+      matter(await readFile(`./pages/projects/${params.id}.md`, "utf8"));
 
   const mdxSource = await renderToString(content);
 
   return {
-    props: {
+    props : {
       mdxSource,
-      frontMatter: data,
+      frontMatter : data,
     },
   };
 }
 
 export async function getStaticPaths() {
   return {
-    paths: ["/projects/inverted-index"],
-    fallback: false,
+    paths : [ "/projects/inverted-index" ],
+    fallback : false,
   };
 }
 
-const Project = ({ mdxSource, frontMatter }) => {
+const Project = ({mdxSource, frontMatter}) => {
   const content = hydrate(mdxSource, {});
-  return (
-    <>
-      <Head>
-        <title>{frontMatter.title}</title>
+  return (<><Head><title>{frontMatter.title}<
+          /title>
         <meta
           name="description"
           content={
@@ -40,10 +37,9 @@ const Project = ({ mdxSource, frontMatter }) => {
             `Isitha Subasinghe ${frontMatter.title} programming project`
           }
         />
-      </Head>
+          </Head>
       {content}
-    </>
-  );
+    </>);
 };
 
 export default Project;
