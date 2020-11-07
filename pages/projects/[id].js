d4f6d784d232853d9a6ea6a77c06318d562617d0
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2020-11-07 14:39:44 +1100

    added highlighting

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,17 +1,60 @@
 import hydrate from "next-mdx-remote/hydrate";
 import Head from "next/head";
+import HighlightPrism, { defaultProps } from "prism-react-renderer";
+import github from "prism-react-renderer/themes/github";
+import TeX from "@matejmazur/react-katex";
 import Back from "../../components/back";
+
+const Highlight = ({
+  children: {
+    props: { className, children },
+  },
+}) => {
+  const language = className.replace(/language-/, "") || "";
+  const newProps = defaultProps;
+  newProps.theme = github;
+
+  console.log(language);
+
+  if (language === "latex") {
+    return <TeX block>{children}</TeX>;
+  }
+  return (
+    <HighlightPrism {...newProps} code={children} language={language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style }}>
+          {tokens.map((line, index) => {
+            const lineProps = getLineProps({ line, key: index });
+            return (
+              <div key={index} {...lineProps}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            );
+          })}
+        </pre>
+      )}
+    </HighlightPrism>
+  );
+};
 
 export async function getStaticProps({ params }) {
   const matter = require("gray-matter");
   const { readFile } = require("fs").promises;
+  const rehypePrism = require("@mapbox/rehype-prism");
+  const rePrism = rehypePrism();
   const renderToString = require("next-mdx-remote/render-to-string");
 
   const { content, data } = matter(
     await readFile(`./pages/projects/${params.id}.md`, "utf8")
   );
 
-  const mdxSource = await renderToString(content);
+  const mdxSource = await renderToString(content, {
+    components: {
+      pre: Highlight,
+    },
+  });
 
   return {
     props: {
@@ -29,7 +72,11 @@ export async function getStaticPaths() {
 }
 
 const Project = ({ mdxSource, frontMatter }) => {
-  const content = hydrate(mdxSource, {});
+  const content = hydrate(mdxSource, {
+    components: {
+      pre: Highlight,
+    },
+  });
   return (
     <>
       <Head>
