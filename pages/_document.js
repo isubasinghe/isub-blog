338db9fbc7bdc4620e6519c1a9c6b40dd1b34632
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-01-10 17:33:13 +1100

    feat: added script for analytics

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class CustomDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="dns-prefetch" href="https://wiki.isub.dev" />
+          <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script> 
         </Head>
         <body>
           <Main />
