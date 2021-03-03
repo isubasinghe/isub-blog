330c4be8998264699a131991f5ae38ac6859d57b
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-03-04 00:59:33 +1100

    feat: started on clock sync

---
+++ pages/test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Clock from "../components/clock";
+
+
+
+const Test = () => {
+    
+    return (
+        <>
+            <Clock id={1} colour="yellow"/>
+        </>
+    )
+};
+
+export default Test;
