b2839de6a6334c5091f16fbbf7adb7f7a82d0daa
isubasinghe <isubasinghe@student.unimelb.edu.au>
2021-03-11 16:56:32 +1100

    feat: added initial files for gossip protocol emulation

---
+++ components/gossip/index.tsx
@@ -0,0 +1,178 @@
+import React, { useState } from "react";
+import Graph from "react-graph-vis";
+import { useForm } from "react-hook-form";
+import { useInterval } from "react-use";
+
+type Edge = {
+  from: number;
+  to: number;
+};
+
+type Node = {
+  id: number;
+  label: string;
+  state: boolean;
+};
+
+type NodeInput = {
+  id: number;
+  type: "add" | "remove";
+};
+
+type EdgeInput = {
+  to: number;
+  from: number;
+  type: "add" | "remove";
+};
+
+const options = {
+  edges: {
+    color: "#a0c1fd",
+  },
+  height: "500px",
+};
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const Gossip = () => {
+  const [nodes, setNodes] = useState<Node[]>([
+    { id: 0, state: true, label: "Node 0" },
+  ]);
+  const [edges, setEdges] = useState<Edge[]>([]);
+  const { register, handleSubmit } = useForm<NodeInput>();
+
+  const {
+    register: registerEdge,
+    handleSubmit: handleSubmitEdge,
+  } = useForm<EdgeInput>();
+
+  const onSubmit = (data: NodeInput) => {
+    setNodes((old) => {
+      const has = old.some((elem) => elem.id === data.id);
+      if (has && data.type === "add") {
+        return old;
+      } else if (has && data.type === "remove" && data.id !== 0) {
+        return [...old].filter((elem) => elem.id !== data.id);
+      } else {
+        return data.type === "add"
+          ? [...old, { id: data.id, state: true, label: `Node ${data.id}` }]
+          : [...old];
+      }
+    });
+  };
+
+  const onEdgeSubmit = (data: EdgeInput) => {
+    setEdges((old) => {
+      const has = old.some(
+        (elem) => elem.to === data.to && elem.from === data.from
+      );
+      if (has && data.type === "add") {
+        return old;
+      } else if (has && data.type === "remove") {
+        return [...old].filter(
+          (elem) => elem.from !== data.from && elem.to !== data.to
+        );
+      } else {
+        return data.type === "add"
+          ? [...old, { to: data.to, from: data.from }]
+          : old;
+      }
+    });
+  };
+
+  const events = {
+    select: function (event) {
+      var { nodes, edges } = event;
+    },
+  };
+  const graph = { nodes, edges };
+
+  useInterval(() => {
+    const index = randomIntFromInterval(0, nodes.length);
+    const node = nodes[index];
+    edges.forEach((edge) => {
+      if (edge.from === node.id) {
+      }
+    });
+  }, 500);
+  return (
+    <>
+      <style>{`
+        .form-container {
+            border: 1px solid black;
+            padding: 10px;
+            width: 45%;
+        }
+        .forms-container {
+            display: flex;
+            justify-content: space-between;
+        }
+        @media only screen and (max-width: 600px){
+            .forms-container {
+                flex-direction: column;
+            }
+            .form-container {
+                width: auto;
+            }
+        }
+      `}</style>
+      <div className="forms-container">
+        <form className="form-container" onSubmit={handleSubmit(onSubmit)}>
+          <label>Node Management</label>
+          <hr />
+          <p>
+            Id:
+            <input
+              name="id"
+              type="number"
+              ref={register({ required: true, valueAsNumber: true })}
+            />
+          </p>
+          <select name="type" ref={register}>
+            <option value="add">Add</option>
+            <option value="remove">Remove</option>
+          </select>
+          <div>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+        <form
+          className="form-container"
+          onSubmit={handleSubmitEdge(onEdgeSubmit)}
+        >
+          <label>Edge Management</label>
+          <hr />
+          <p>
+            To:
+            <input
+              name="to"
+              type="number"
+              ref={registerEdge({ required: true, valueAsNumber: true })}
+            />
+          </p>
+          <p>
+            From:
+            <input
+              name="from"
+              type="number"
+              ref={registerEdge({ required: true, valueAsNumber: true })}
+            />
+          </p>
+          <select name="type" ref={registerEdge}>
+            <option value="add">Add</option>
+            <option value="remove">Remove</option>
+          </select>
+          <div>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+      <Graph graph={graph} options={options} events={events} />
+    </>
+  );
+};
+
+export default Gossip;
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,19 +1,21 @@
-import React, {useReducer} from "react";
-import dynamic from 'next/dynamic'
+import React, { useReducer } from "react";
+import dynamic from "next/dynamic";
 
 const BerkeleyClockSync = dynamic(
-    () => import('../components/clock/berkeley'),
-    { loading: () => <p>...</p> }
-)
+  () => import("../components/clock/berkeley"),
+  { loading: () => <p>...</p> }
+);
 
+const Gossip = dynamic(() => import("../components/gossip"), {
+  loading: () => <p>...</p>,
+});
 
 const Test = () => {
-
-    return (
-        <>
-            <BerkeleyClockSync />
-        </>
-    )
+  return (
+    <>
+      <Gossip />
+    </>
+  );
 };
 
 export default Test;
