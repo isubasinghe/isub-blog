6357c556df4cd316e67dfe2e84d0b44790b5a1a2
Restyled.io <commits@restyled.io>
2021-04-02 02:45:41 +0000

    Restyled by clang-format

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ThemeProvider } from "theme-ui";
+import Document, {Head, Html, Main, NextScript} from "next/document";
+import {ThemeProvider} from "theme-ui";
+
 import theme from "../theme";
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    return {...initialProps};
   }
 
   render() {
