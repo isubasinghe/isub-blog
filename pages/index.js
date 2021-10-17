016525db620b9d63b90da8e6a8d99f6c627b0942
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-10-17 12:22:47 +1100

    feat: added update

--- pages/index.js
+++ pages/index.js
@@ -32,19 +32,46 @@ const Home = () => (
       Most of the code I hack around with is open source, 
       you can have a look <a style={{fontSize:"16px"}} href="https://github.com/isubasinghe">here</a>
     </p>
-    <h3>Programming Languages</h3>
+    <p><strong>This following section is mainly aimed at recruiters so they can develop an understanding of what I am like as a programmer and what values I bring into their engineering organisation.</strong></p>
+    <h3>Engineering</h3>
     <ul>
-      <li>C++</li>
-      <li>C</li>
-      <li>Haskell</li>
+      <li>We have an obligation not to waste resources - tech like Electron are bad</li>
+      <li>Performance is crucial - don't make the user wait</li>
+      <li>Correctness is crucial - having at least some knowledge of formal verification is good</li>
+      <li>Avoid non determinism where possible</li>
+      <li>Write simple code</li>
+      <li>If it can be done at compile time, it should be done at compile time</li>
+    </ul>
+    <p>Unfortunately, the above criteria do not come in isolation, most of the time it is a balance and what that balance looks like is domain dependant.</p>
+    <h3>Programming Languages - Preferred Languages in Bold</h3>
+    I generally prefer languages with strong types, as this makes programming easier for me. Being able to write programs that can be 
+    verified at compile time is a huge help. C++ (I should elaborate more here) and C do not exactly fall into this category but I still like these languages for perhaps more sentimental reasons.
+    <ul>
+      <li><strong>C++</strong></li>
+      <li><strong>C</strong></li>
+      <li><strong>Haskell</strong></li>
+      <li><strong>OCaml</strong></li>
       <li>Python</li>
       <li>Go</li>
       <li>Java</li>
-      <li>Rust</li>
+      <li><strong>Rust</strong></li>
       <li>JavaScript</li>
       <li>TypeScript</li>
-      <li>x86-64 (To a certain extent) </li>
+      <li>x86-64 Assembly </li>
+      <li>RISC-V Assembly </li>
     </ul>
+    <h3>Programming languages I am learning</h3>
+    <ul>
+      <li>Idris - Dependant types for formal verifcation is neat</li>
+      <li>Racket - So much hype including comments such as "god wrote the universe in lisp"</li>
+    </ul>
+    <h3>Other</h3>
+    <ul>
+      <li>TLA+ - Proving distributed systems is important</li>
+      <li>PlusCal - Attempting to learn since TLA+ is difficult </li>
+      <li>Dafny - formal verification </li>
+    </ul>
+
   </div>
 );
 
