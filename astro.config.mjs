a059700882963dd66ffe3d355fbf2505a88c0dc9
isubasinghe <isitha@pipekit.io>
2026-04-12 00:00:27 +1000

    feat: add vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   integrations: [solidJs(), mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
